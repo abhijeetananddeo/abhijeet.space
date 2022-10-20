e408d4974d9180dfa1c0fa8fbb8843a12b36c07f
abhijeetananddeo <after18abhijeet@gmail.com>
2022-10-20 14:00:59 +0530

    inital commit

---
+++ app.js
@@ -0,0 +1,9 @@
+(function(){
+    var element=document.getElementsByTagName('p');
+  setInterval(function(){
+      if((element[0].clientWidth)==380) {
+        element[1].classList.add('f1');
+      }
+    
+  }, 1000)
+  }());
