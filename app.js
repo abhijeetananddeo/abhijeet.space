05ccbccb37c9515724884629615aa4ee72c8aa85
abhijeetananddeo <after18abhijeet@gmail.com>
2022-10-25 01:42:08 +0530

    made the grid, used custom font, added social media links

--- app.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    var element=document.getElementsByTagName('p');
-  setInterval(function(){
-      if((element[0].clientWidth)==380) {
-        element[1].classList.add('f1');
-      }
-    
-  }, 1000)
-  }());
